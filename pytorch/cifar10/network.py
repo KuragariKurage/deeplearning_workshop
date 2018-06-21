ea68adc9c263ac3984d0e9530ae4cea5b32d71f1
Atsuya Sakata <atsuya22@gmail.com>
2018-06-21 14:31:22 +0900

    implemented cifar10 (not debugged)

---
+++ pytorch/cifar10/network.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CifarConvNet(nn.Module):
+    def __init__(self):
+        super(CifarConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 5)
+        self.conv2 = nn.Conv2d(64, 64, 5)
+        self.fc1 = nn.Linear(64, 384)
+        self.fc1_dropout = nn.Dropout()
+        self.fc2 = nn.Linear(384, 192)
+        self.fc2_dropout = nn.Dropout()
+        self.classification = nn.Linear(192, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.classification(x)
+
+        return F.softmax(x)

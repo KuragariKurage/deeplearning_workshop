3071583ec1ed7ea00cadbe56ec42c61a37cdd89c
Atsuya Sakata <atsuya22@gmail.com>
2018-06-21 15:23:14 +0900

    implemented predict by classes and refactor a little

--- pytorch/cifar10/main.py
+++ pytorch/cifar10/main.py
@@ -18,9 +18,9 @@ def arguments():
                         help='batch size for validation')
     parser.add_argument('--epochs', default=10, type=int, metavar='N',
                         help='number of epochs to train')
-    parser.add_argument('--lr', default=0.01, type=float, metavar='LR',
+    parser.add_argument('--lr', default=0.001, type=float, metavar='LR',
                         help='learning rate')
-    parser.add_argument('--momentum', default=0.5, type=float,
+    parser.add_argument('--momentum', default=0.9, type=float,
                         help='SGD momentum')
     parser.add_argument('--no-cuda', default=False,
                         action='store_true', help='disable CUDA training')
@@ -34,22 +34,22 @@ def arguments():
     return args
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(args, model, device, train_loader, optimizer, criterion, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.cross_entropy(output, target)
+        loss = criterion(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % args.log_interval == (args.log_interval - 1):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
 
-def valid(args, model, device, valid_loader):
+def valid(args, model, device, valid_loader, criterion):
     model.eval()
     val_loss = 0
     correct = 0
@@ -57,8 +57,8 @@ def valid(args, model, device, valid_loader):
         for data, target in valid_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            val_loss += F.cross_entropy(output,
-                                        target, size_average=False).item()
+            val_loss += criterion(output,
+                                  target, size_average=False).item()
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).sum().item()
 
@@ -66,6 +66,29 @@ def valid(args, model, device, valid_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         val_loss, correct, len(valid_loader.dataset),
         100. * correct / len(valid_loader.dataset)))
+
+
+def predict_classes(model, device, test_loader, criterion):
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    model.eval()
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data, labels in test_loader:
+            outputs = model(data)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(10):
+        print('Accuracy of {:.5s} : {:.2d} %'.format(
+            classes[i], 100 * class_correct[i] / class_total[i]
+        ))
 
 
 def main():
@@ -95,12 +118,15 @@ def main():
     )
 
     model = CifarConvNet().to(device)
+    criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=args.lr,
                           momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        valid(args, model, device, valid_loader)
+        train(args, model, device, train_loader, optimizer, criterion, epoch)
+        valid(args, model, device, valid_loader, criterion)
+
+    predict_classes(model, device, valid_loader, criterion)
 
 
 if __name__ == '__main__':

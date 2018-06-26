fbcd319037039c091b18f59d074e117dab4ecf24
Atsuya Sakata <atsuya22@gmail.com>
2018-06-26 18:24:45 +0900

    moved dropout to forward()

--- pytorch/mnist/network.py
+++ pytorch/mnist/network.py
@@ -8,13 +8,14 @@ class ConvNet(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, 5)
         self.conv2 = nn.Conv2d(32, 64, 5)
         self.linear1 = nn.Linear(64, 1024)
-        self.lin1_dropout = nn.Dropout()
+        # self.lin1_dropout = nn.Dropout()
         self.classification = nn.Linear(1024, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
         x = x.view(x.size(0), -1)
+        x = F.dropout(x)
         x = self.classification(x)
 
         return x

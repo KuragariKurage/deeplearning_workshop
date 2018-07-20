5598c51c3737301e53dca14264fac69b7b590336
Atsuya Sakata <atsuya22@gmail.com>
2018-07-20 11:24:18 +0900

    Dropout直し

--- pytorch/cifar10/network.py
+++ pytorch/cifar10/network.py
@@ -8,17 +8,15 @@ class CifarConvNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, 5)
         self.conv2 = nn.Conv2d(64, 64, 5)
         self.fc1 = nn.Linear(64 * 5 * 5, 384)
-        self.fc1_dropout = nn.Dropout()
         self.fc2 = nn.Linear(384, 192)
-        self.fc2_dropout = nn.Dropout()
         self.classification = nn.Linear(192, 10)
 
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), 2)
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = F.dropout(F.relu(self.fc1(x)))
+        x = F.dropout(F.relu(self.fc2(x)))
         x = self.classification(x)
 
         return x
